35518b2a2aa355be4a10eb44f6df9122618dced1
blakfreeman <61966557+blakfreeman@users.noreply.github.com>
2021-05-10 10:37:50 +0200

    start

---
+++ main.js
@@ -0,0 +1,26 @@
+let stockH1 = document.getElementsByTagName('h1');
+
+console.log(stockH1);
+
+let sotckP = document.getElementsByTagName('p');
+
+
+let sotckh3 = document.getElementsByTagName('h3');
+
+let sotckh3One = stockH1[0];
+let sotckh3Two = stockH1[1];
+let sotckh3Tree = stockH1[2];
+
+console.log(sotckh3One);
+console.log(sotckh3Two);
+console.log(sotckh3Tree);
+
+let p2 = sotckP[1];
+
+console.log(p2);
+
+let li = document.getElementsByTagName('li');
+
+let li3 = li[2];
+console.log(li3);
+ 
